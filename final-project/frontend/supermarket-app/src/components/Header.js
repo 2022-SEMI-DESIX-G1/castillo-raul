f3cb7a571dd25c83c0d8e282d23e12bf6f485675
Raul Castillo <raulincastle@gmail.com>
2022-07-21 21:05:09 -0500

    FrontEnd Supermarket OK - Pending login page and integration with backend

---
+++ final-project/frontend/supermarket-app/src/components/Header.js
@@ -0,0 +1,91 @@
+import {
+  Badge,
+  Button,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+
+const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+    productDispatch,
+  } = CartState();
+
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">SuperMarket</Link>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search a Product"
+            className="m-auto"
+            onChange={(e) =>
+              productDispatch({
+                type: "FILTER_BY_SEARCH",
+                payload: e.target.value,
+              })
+            }
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown align={{ sm:"right" }}>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25" />
+              <Badge bg='none'>{cart.length}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidht: 370 }}>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((product) => (
+                    <span className="cart-item" key={product.id}>
+                      <img
+                        src={product.image}
+                        className="cart-item-img"
+                        alt={product.name}
+                      />
+                      <div className="cart-item-detail">
+                        <span>{product.name}</span>
+                        <span>${product.price}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: product,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart!
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
